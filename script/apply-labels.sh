fa5cb95d7f8421c6e79db8be3f7b48b2eb4a5075
Chilensis <chilensis.dev@gmail.com>
2025-03-04 22:53:37 +0900

    Update apply-labels.sh

--- script/apply-labels.sh
+++ script/apply-labels.sh
@@ -13,7 +13,7 @@ REPOS=$(curl -s -H "Authorization: Bearer $GH_TOKEN" \
 for REPO in $REPOS; do
   echo "Syncing labels for repository: $REPO"
   
-  npx github-label-sync --labels .github/labels.json --org "$ORG_NAME" --repo "$REPO" --token "$GH_TOKEN"
+  npx github-label-sync --labels script/labels.json --org "$ORG_NAME" --repo "$REPO" --token "$GH_TOKEN"
 
   echo "Labels synchronized for $REPO"
 done
